4da31fe27d845439d56464fd27a62ba89264aaf4
JoseAngelRn <joseangel.ma44965@cesurformacion.com>
2022-02-14 10:25:10 +0100

    Validacion creada

--- Valida.php
+++ Valida.php
@@ -3,7 +3,14 @@ INCLUDE_ONCE 'Funciones_Aeropuerto.php';
 
 #REPRESENTACIÓN
 echo "<body style='background-color: rgb(227, 230, 233);'></body>";
-echo "<h2 style='color:#2d4b6e;'>"."Las estadisticas de las ciudades son: "."</h2>";
+echo "<h2 style='color:#2d4b6e;'>"."Validación"."</h2>";
 
+$validaEmail = $_POST["Email"];
+
+if (filter_var($validaEmail, FILTER_VALIDATE_EMAIL)) {
+    echo "EL correo es valido";
+}else{
+    echo "El correo NO es valido";
+}
 
 ?>
